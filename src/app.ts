7a1066e686d2ef0c41bf830708a5e4b480b7bf97
kazirauf <raufelahi9876@gmail.com>
2024-10-27 19:28:29 +0600

    update links

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'https://kre-elite-sport.vercel.app'],
+    origin: ['http://localhost:5173', 'https://kre-elite-sport-client.vercel.app'],
     credentials: true,
   }),
 );
